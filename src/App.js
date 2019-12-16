d24315f7e52b7d37049d7edf1778711cc5e6f6be
Wayne Sun <wtse.sun@gmail.com>
2019-12-16 14:34:05 -0800

    built function for play the note when key clicked

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import notes from './NotesCollector'
+
+function playSound(e) {
+  let index = e.target.dataset.note;
+  let noteForPlay = new Audio(notes[index])
+  noteForPlay.play();
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="key white" data-note='0' onClick={(e) => {playSound(e)}}></div>
+     <div className="key black" data-note='1' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='2' onClick={(e) => {playSound(e)}}></div>
+     <div className="key black" data-note='3' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='4' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='5' onClick={(e) => {playSound(e)}}></div>
+     <div className="key black" data-note='6' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='7' onClick={(e) => {playSound(e)}}></div>
+     <div className="key black" data-note='8' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='9' onClick={(e) => {playSound(e)}}></div>
+     <div className="key black" data-note='10' onClick={(e) => {playSound(e)}}></div>
+     <div className="key white" data-note='11' onClick={(e) => {playSound(e)}}></div>
+
     </div>
   );
 }
---
+++ src/NotesCollector.js
@@ -0,0 +1,32 @@
+import A from './notes/A.mp3'
+import Ab from './notes/Ab.mp3'
+import B from './notes/B.mp3'
+import Bb from './notes/Bb.mp3'
+import C from './notes/C.mp3'
+import D from './notes/D.mp3'
+import Db from './notes/Db.mp3'
+import E from './notes/E.mp3'
+import Eb from './notes/Eb.mp3'
+import F from './notes/F.mp3'
+import G from './notes/G.mp3'
+import Gb from './notes/Gb.mp3'
+
+let notes = []
+
+
+notes.push(C)
+notes.push(Db)
+notes.push(D)
+notes.push(Eb)
+notes.push(E)
+
+notes.push(F)
+notes.push(Gb)
+notes.push(G)
+notes.push(Ab)
+notes.push(A)
+notes.push(Bb)
+notes.push(B)
+
+
+export default notes
